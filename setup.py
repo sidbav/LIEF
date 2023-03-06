c5fec4920cb2ed0ab4b11e41fb5797fe23034ee9
Sidharth Baveja <sidharthbav@gmail.com>
2023-03-06 06:41:37 -0600

    trying it out

--- setup.py
+++ setup.py
@@ -7,16 +7,5 @@ try:
 except ImportError:
     from distutils.core import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="UTF-8") as f:
-    readme = f.read()
-
-version = "0.9.0"
-package_data = {}
-setup(
-    name="lief",
-    version=version,
-    description="Something"
-    long_description=readme,
-    packages=["lief"],
-    package_data=package_data,
-    author_email="proth@endgame.com")
+if __name__ == "__main__":
+    setup()
